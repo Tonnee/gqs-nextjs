9f9d6dd1ca5ba7cb0f6b5a59042e815cf0a9cf9f
Sabiha Afrin <satonnee@gmail.com>
2026-02-16 20:10:57 +0600

    feat(layout): introduce Container and integrate Navbar into root layout
    Adds reusable Container component for consistent spacing.
    Wraps Navbar with Container and mounts Navbar inside main layout file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,39 @@
 import type { Metadata } from "next";
 import { Poppins, Raleway } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar/Navbar";
 
 const poppins = Poppins({
-  variable: "--font-poppins",
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
-  display: "swap",
+    variable: "--font-poppins",
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+    display: "swap",
 });
 
 const raleway = Raleway({
-  variable: "--font-raleway",
-  subsets: ["latin"],
-  display: "swap",
+    variable: "--font-raleway",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "GRE Quant School",
-  description: "GRE Quant School",
+    title: "GRE Quant School",
+    description: "GRE Quant School",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${poppins.variable} ${raleway.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${poppins.variable} ${raleway.variable} antialiased`}
+            >
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Container } from "../layout/Container";
+
+export function Navbar() {
+    return (
+        <nav>
+            <Container>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Mollitia maxime accusantium a provident praesentium libero quas
+                aliquid necessitatibus doloribus eligendi quisquam hic minima
+                repudiandae enim veniam dolorem, quos eaque doloremque ex eum,
+                atque voluptatum laudantium rerum. Ex sunt blanditiis
+                praesentium tenetur repudiandae facere, nesciunt soluta
+                temporibus ipsum architecto quod deserunt saepe voluptatibus
+                enim id non veniam ullam. Accusamus aliquid fuga illum illo est
+                quaerat rerum magni vero quod sunt quas atque impedit molestiae,
+                qui placeat eum? Quos iusto doloribus, quibusdam cupiditate
+                facilis animi pariatur autem sed vel molestias! Facilis numquam
+                temporibus ipsam incidunt et esse maiores nisi sapiente dolor
+                consectetur?
+            </Container>
+        </nav>
+    );
+}
